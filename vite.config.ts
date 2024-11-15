867b4f07ddcbfad073c15214565f36f0fb627f3f
Madusha Thilakarathna <mail@madusha.me>
2024-11-15 21:25:33 +0700

    feat: setup routing.

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,27 @@
-import path from "node:path"
-import { defineConfig } from "vite"
+import path from "node:path";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
+		TanStackRouterVite(),
 		react(),
 		VitePWA({
-			registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
+			registerType: "autoUpdate",
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+			},
 			devOptions: {
 				enabled: true,
 			},
 		}),
 	],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
