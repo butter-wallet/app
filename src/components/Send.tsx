53f50656feb1387a1f85f8e516610f51863844d2
yathindrak <yathindrarawya123@gmail.com>
2024-11-16 03:58:46 +0700

    feat: basic wallet ui

--- src/components/Send.tsx
+++ src/components/Send.tsx
@@ -1,3 +1,20 @@
-export const Send = () => {
-	return <div>Send</div>;
-};
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerTrigger, DrawerContent } from "@/components/ui/drawer";
+import SendDrawerContent from './SendDrawerContent';
+
+export default function Send() {
+  return (
+    <div className="flex items-center justify-center">
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button size="lg">
+            Send
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <SendDrawerContent />
+        </DrawerContent>
+      </Drawer>
+    </div>
+  );
+}
