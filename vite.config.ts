7cf05ba97893e5650beaf2071120f2bf962ee38f
Madusha Thilakarathna <mail@madusha.me>
2024-11-15 21:18:32 +0700

    feat: initialize shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from "vite";
+import path from "node:path"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
@@ -16,4 +17,9 @@ export default defineConfig({
 			},
 		}),
 	],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
